63c803fbb0ce4bb2d7297b7e50d5aed5c3c3bf9d
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2022-04-30 18:24:52 -0400

    k

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,14 @@
 import firebase from "firebase";
 
-const firebaseConfig = {/*Use your own Config*/};
+const firebaseConfig = {
+    apiKey: "AIzaSyDG_Wn15OJG6hcjp3qeJ2f659lx-_llZ8w",
+    authDomain: "dragonhacks22.firebaseapp.com",
+    projectId: "dragonhacks22",
+    storageBucket: "dragonhacks22.appspot.com",
+    messagingSenderId: "618608151728",
+    appId: "1:618608151728:web:da008998ef25be2c3b3f75",
+    measurementId: "G-6M3QSG4JSL"
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
